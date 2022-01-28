16d9cacae5de0b9805eff8a7e61d05327db69b5b
Aqil <91294196+v6k@users.noreply.github.com>
2022-01-28 11:39:37 -0700

    Final commit

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 #  coding: utf-8 
+import mimetypes
 import socketserver
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+# Copyright 2022 v6k
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -30,9 +31,40 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        #Converting b'request....' into string: https://www.javatpoint.com/how-to-convert-bytes-to-string-in-python
+        self.data = self.request.recv(1024).strip().decode()
+
+        method = self.data.split('\r\n')[0].split(' ')[0]
+        browserPath = self.data.split('\r\n')[0].split(' ')[1]
+        header=""
+        file = ""
+        if method != "GET":
+            self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed\r\n",'utf-8'))
+        else:
+            try:           
+                if browserPath[-1] == "/":
+                    browserPath += "index.html"
+                actualFilePath = open("www" + browserPath, "rb")
+                file = actualFilePath.read()
+                #https://stackoverflow.com/questions/4219020/how-do-i-find-mime-type-in-python/4219188#4219188
+                header = "HTTP/1.1 200 OK\r\nContent-Type:"+ mimetypes.guess_type(browserPath)[0]+"\r\n\r\n"
+            except FileNotFoundError:
+                header = "HTTP/1.1 404 Not Found\r\n\r\n"
+            except IsADirectoryError:
+                header = "HTTP/1.1 301 Moved Permanently\r\n\r\n"
+                self.request.sendall(bytearray(header,'utf-8'))
+                self.request.sendall(bytearray("Location: "+browserPath+"/", 'utf-8'))
+                return    
+            except TypeError:
+                header = "HTTP/1.1 404 Not Found\r\n\r\n"
+                self.request.sendall(bytearray(header,'utf-8'))
+                return
+            
+        self.request.sendall(bytearray(header,'utf-8'))
+        self.request.sendall(file) if file!="" else self.request.sendall(bytearray(file,'utf-8'))
+        self.request.close()
+
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
